762b4a99d17e022b5a4ba2244d9db8c742d85338
alaaateff <alaaatef3200@gmail.com>
2026-06-14 02:04:59 +0300

    modified the data ebs assume role

--- Terraform/modules/gather.tf
+++ Terraform/modules/gather.tf
@@ -1,21 +1,20 @@
 data "tls_certificate" "eks-certificate" {
   url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
 }
-data "aws_iam_policy_document" "eks-oidc-assume_role_policy" {
 
+data "aws_iam_policy_document" "ebs_assume_role" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.eks_oidc.arn]
+    }
 
     condition {
       test     = "StringEquals"
-      variable = "${replace(aws_iam_openid_connect_provider.eks_oidc.url,"https://", "")}:sub"
-      values = ["system:serviceaccount:default:aws-test"]
-    }
-
-    principals {
-        identifiers = [ aws_iam_openid_connect_provider.eks_oidc.arn]
-        type = "Federated"
+      variable = "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
     }
   }
 }

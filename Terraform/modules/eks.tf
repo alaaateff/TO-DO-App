1274c9d652071495f60bfcfca9fc710f05165be9
alaaateff <alaaatef3200@gmail.com>
2026-06-14 02:06:07 +0300

    seperated ebs addon from the rest to assign to it the role

--- Terraform/modules/eks.tf
+++ Terraform/modules/eks.tf
@@ -32,6 +32,18 @@ resource "aws_eks_addon" "eks-addons" {
   cluster_name                = aws_eks_cluster.eks.name
   addon_name                  = each.value.name
   addon_version               = each.value.version
+  depends_on = [
+    aws_eks_node_group.ondemand-node,
+    aws_eks_node_group.spot-node
+  ]
+}
+
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name  = aws_eks_cluster.eks.name
+  addon_name    = "aws-ebs-csi-driver"
+  addon_version = "v1.46.0-eksbuild.1"
+
+  service_account_role_arn = aws_iam_role.ebs_csi_irsa.arn
 
   depends_on = [
     aws_eks_node_group.ondemand-node,

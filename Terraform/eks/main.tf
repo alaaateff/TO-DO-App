7baf20bf2e801b8a6856856a99a12aa32a1b1581
alaaateff <alaaatef3200@gmail.com>
2026-06-17 21:15:50 +0300

    added policy and roles for alb controller,secret manager,added ecr repos resources

--- Terraform/eks/main.tf
+++ Terraform/eks/main.tf
@@ -22,3 +22,129 @@ module "eks" {
     max_capacity_spot = var.max_capacity_spot
     spot_instance_types = var.spot_instance_types 
 }
+
+resource "aws_iam_policy" "aws_load_balancer_controller_policy" {
+  name        = "aws_load_balancer_controller_policy"
+  path        = "/"
+  policy = file("../../iam_policy.json")
+}
+
+resource "aws_iam_role" "aws_load_balancer_controller_role" {
+  name = "aws_load_balancer_controller_role"
+
+  assume_role_policy = jsonencode({
+    "Version":"2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "arn:aws:iam::214519213041:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/61C64AECD99B1D7CE27467C58B919F6D"
+            },
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Condition": {
+                "StringEquals": {
+                    "oidc.eks.us-east-1.amazonaws.com/id/61C64AECD99B1D7CE27467C58B919F6D:aud": "sts.amazonaws.com",
+                    "oidc.eks.us-east-1.amazonaws.com/id/61C64AECD99B1D7CE27467C58B919F6D:sub": "system:serviceaccount:kube-system:aws-load-balancer-controller"
+                }
+            }
+        }
+    ]
+})
+}
+
+resource "aws_iam_role_policy_attachment" "aws_load_balancer_controller_attachement" {
+  role       = aws_iam_role.aws_load_balancer_controller_role.name
+  policy_arn = "arn:aws:iam::214519213041:policy/aws_load_balancer_controller_policy"
+}
+
+
+
+resource "aws_ecr_repository" "frontend_repo" {
+  name = "frontend-repo"
+    image_tag_mutability = "IMMUTABLE"
+    image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "backend_repo" {
+  name = "backend-repo"
+    image_tag_mutability = "IMMUTABLE"
+    image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_kms_key" "kms_key" {
+  description             = "KMS key for database credentials rotation"
+  enable_key_rotation     = true
+  deletion_window_in_days = 7
+}
+
+resource "aws_secretsmanager_secret" "db_credentials" {
+  name = "database-credentials"
+  kms_key_id = aws_kms_key.kms_key.key_id
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "db_credentials_version" {
+  secret_id     = aws_secretsmanager_secret.db_credentials.id
+  secret_string = jsonencode({
+    username = var.db_username,
+    password = var.db_password
+  })
+}
+resource "aws_iam_policy" "external_secrets_policy" {
+  name = "external-secrets-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:ListSecrets"
+        ],
+        Resource = "arn:aws:secretsmanager:us-east-1:214519213041:secret:database-credentials*"
+      },
+      {
+          Effect = "Allow",
+          Action = [
+               "kms:Decrypt",
+               "kms:DescribeKey"
+                ],
+          Resource = aws_kms_key.kms_key.arn
+}
+    ]
+  })
+}
+
+resource "aws_iam_role" "external_secret_operator_role" {
+  name = "external_secret_operator_role"
+
+  assume_role_policy = jsonencode({
+    "Version":"2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Federated": "arn:aws:iam::214519213041:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/61C64AECD99B1D7CE27467C58B919F6D"
+            },
+            "Action": "sts:AssumeRoleWithWebIdentity",
+            "Condition": {
+                "StringEquals": {
+                    "oidc.eks.us-east-1.amazonaws.com/id/61C64AECD99B1D7CE27467C58B919F6D:aud": "sts.amazonaws.com",
+                    "oidc.eks.us-east-1.amazonaws.com/id/61C64AECD99B1D7CE27467C58B919F6D:sub": "system:serviceaccount:external-secrets:external-secret-operator"
+                }
+            }
+        }
+    ]
+})
+}
+
+resource "aws_iam_role_policy_attachment" "external_secret_operator_attachement" {
+  role       = aws_iam_role.external_secret_operator_role.name
+  policy_arn = aws_iam_policy.external_secrets_policy.arn
+}
